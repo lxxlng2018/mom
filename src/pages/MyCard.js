dc0c45169c24e59ea3c2efa4c16626c7ab1e405a
zengchang <3454278735@qq.com>
2018-05-05 12:04:01 +0800

    asdasdasd

--- src/pages/MyCard.js
+++ src/pages/MyCard.js
@@ -3,7 +3,7 @@
  */
 import React, { Component } from 'react';
 import moment from 'moment';
-import { createForm } from 'rc-form'
+import UploadImage from 'react-upload-images';
 import {
     Flex,
     WhiteSpace,
@@ -20,7 +20,8 @@ import {
     TextareaItem,
     Tag,
     Picker,
-    InputItem
+    InputItem,
+    Toast
 } from 'antd-mobile';
 import FirendService from '../service/FirendService'
 const Item = List.Item
@@ -29,7 +30,8 @@ export default class MyCard extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            types:[]
+            types:[],
+            data:{}
         }
     }
 
@@ -77,9 +79,18 @@ export default class MyCard extends Component {
     }
 
     handleField = (field,val)=>{
-
+        console.log(field,val)
+        if(!val){
+            return Toast.fail(`请选择内容`)
+        }
+        let {data} = this.state;
+        this.setState({
+            data:{
+                ...data,
+                [field]:val
+            }
+        })
     }
-
 
     render() {
         return <div className="home">
@@ -112,68 +123,70 @@ export default class MyCard extends Component {
                     <Flex className="am-list-item am-input-item am-list-item-middle">
                         <div className="am-input-label" style={{ flex: 1 }}>头像上传</div>
                         <div style={{ flex: 2 }}>
+                            <UploadImage name="imgFile" uploadLink="http://www.669669669.com/kindedit/php/upload_jsone.php" />
                             <Button type="primary" size="small" inline style={{ margin: '0 2px' }}>上传本地照片</Button>
                             <Button type="primary" size="small" inline style={{ margin: '0 5px' }}>选择推荐头像</Button>
                         </div>
                     </Flex>
                     <Picker cols={1} 
                         data={(()=>this.handleGetType('jy_yx'))()}
-                        onOk={(val)=>this.handleField('jx_yx',val)}
+                        onOk={(val)=>this.handleField('jx_yx',val[0])}
+                        value={[this.state.data.jx_yx]}
                         >
                         <Item >交友目的</Item>
                     </Picker>
                     <Picker cols={1} data={(() => this.handleGetType('age_range'))()}
-                        onOk={(val)=>this.handleField('age_range',val)}
+                        onOk={(val)=>this.handleField('age_range',val[0])}
+                        value={[this.state.data.age_range]}
                     >
                         <Item >年龄段</Item>
                     </Picker>
                     <Picker cols={1} data={(() => this.handleGetType('sex'))()}
-                        onOk={(val)=>this.handleField('sex',val)}
+                        onOk={(val)=>this.handleField('sex',val[0])}
+                        value={[this.state.data.sex]}
                     >
                         <Item >性别</Item>
                     </Picker>
                     <Picker cols={1} data={(() => this.handleGetType('SHENGSHI'))()}
-                        onOk={(val)=>this.handleField('province',val)}
+                        onOk={(val)=>this.handleField('province',val[0])}
+                        value={[this.state.data.province]}
                     >
                         <Item >地区</Item>
                     </Picker>
                     <Picker cols={1} data={(() => this.handleGetType('profession'))()}
-                        onOk={(val)=>this.handleField('profession',val)}
+                        onOk={(val)=>this.handleField('profession',val[0])}
+                        value={[this.state.data.profession]}
                     >
                         <Item >行业</Item>
                     </Picker>
                     <Picker cols={1} data={(() => this.handleGetType('character'))()}
-                        onOk={(val)=>this.handleField('character',val)}
+                        onOk={(val)=>this.handleField('character',val[0])}
+                        value={[this.state.data.character]}
                     >
                         <Item >爱好</Item>
                     </Picker>
-                    <InputItem
-                        type="string"
-                        placeholder=""
-                        clear
-                        moneyKeyboardAlign="left">网名</InputItem>
                     <Picker cols={1} data={(() => this.handleGetType('degree'))()}
                         onOk={(val)=>this.handleField('degree',val)}
                     >
                         <Item >学历</Item>
                     </Picker>
-                    <Picker cols={1} data={(() => this.handleGetType('character'))()}>
-                        <Item >学历</Item>
-                    </Picker>
                     <InputItem
                         type="string"
                         placeholder=""
                         clear
+                        onChange={(e)=>this.handleField('phone',e)}
                         moneyKeyboardAlign="left">手机</InputItem>
                     <InputItem
                         type="string"
                         placeholder=""
                         clear
+                        onChange={(e)=>this.handleField('qq',e)}
                         moneyKeyboardAlign="left">QQ</InputItem>
                     <InputItem
                         type="string"
                         placeholder=""
                         clear
+                        onChange={(e)=>this.handleField('weixin',e)}
                         moneyKeyboardAlign="left">微信</InputItem>
                     <Item><Button type="primary">提交</Button></Item>
                 </List>
